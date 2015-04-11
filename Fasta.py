a174d9737c98b42555625a5a70345ff7f1fb9f5f
Kai Yu <finno@live.cn>
2015-04-11 14:48:32 +0800

    Fasta module updated to version 1.3

--- Fasta.py
+++ Fasta.py
@@ -2,104 +2,79 @@
 # Fasta.py
 # Kai Yu
 # Version 1.3 @ 150411
-# Modification: Fixed some bugs with which may calse incorrect result when there is 
-#               empty line in fasta file.
-
-import os, sys, gzip
-class Readfa(object):
+class Parse(object):
+    '''class Parse(fastaPath)
+    return self.id (list)
+           self.seq (dictionary with key in self.id)
+           self.des (dictionary with key in self.id)
+    '''
     def __init__(self, fastaPath):
         self.fastaPath = fastaPath
         if self.fastaPath[-3:] == '.gz':
+            import gzip
             self.file = gzip.open(self.fastaPath,'rb')
         else:
             self.file = open(self.fastaPath, 'r')
         self.id  = []
         self.seq = {}
         self.des = {}
-    def 
+        while 1:
+            line = self.file.readline()
+            if not line:
+                break
+            if line == '':
+                continue
+            if line[0] == '>':
+                break
+        while 1:
+            if not line:
+                break
+            if line == '':
+                continue
+            title = line[1:].rstrip()
+            seqid = title.split()[0]
+            description = title[len(seqid):]
+            seq = ''
+            while 1:
+                line = self.file.readline()
+                if not line:
+                    self.seq[seqid] = seq
+                    self.id.append(seqid)
+                    self.des[seqid] = description
+                    break
+                if line == '':
+                    continue
+                if line[0] != '>':
+                    seq += line.rstrip()
+                elif line[0] == '>':
+                    self.seq[seqid] = seq
+                    self.id.append(seqid)
+                    self.des[seqid] = description
+                    break
+        self.file.close()
 
-# def parse(fastapath):
-#     if fastapath[-3:] == '.gz':
-#         import gzip
-#         infile = gzip.open(fastapath,'rb')
-#     else:
-#         infile = open(fastapath,'r')
-#     fadic = {'order':[],'seq':{},'description':{}}
-#     while True:
-#         line = infile.readline()
-#         if not line:
-#             break
-#         if line == '':
-#             continue
-#         if line[0] == '>':
-#             break
-#     while True:
-#         if not line:
-#             break
-#         if line == '':
-#             continue
-#         title = line[1:].rstrip()
-#         seqid = title.split()[0]
-#         description = title[len(seqid):]
-#         seq = ''
-#         while True:
-#             line = infile.readline()
-#             if not line:
-#                 fadic['seq'][seqid] = seq
-#                 fadic['order'].append(seqid)
-#                 fadic['description'][seqid] = description
-#                 break
-#             if line == '':
-#                 continue
-#             if line[0] != '>':
-#                 seq += line.rstrip()
-#             elif line[0] == '>':
-#                 fadic['seq'][seqid] = seq
-#                 fadic['order'].append(seqid)
-#                 fadic['description'][seqid] = description
-#                 break
-#     infile.close()
-#     return fadic
-# def write(fadic,savepath,orderlist=[],description={}):
-#     if savepath[-3:] == '.gz':
-#         savefile = gzip.open(savepath,'wb')
-#     else:
-#         savefile = open(savepath,'w')
-#     if type(orderlist) == list and len(orderlist) != 0:
-#         for seqid in orderlist:
-#             seq = fadic[seqid]
-#             des = ''
-#             if seqid in description:
-#                 des = description[seqid]
-#             if len(seq) <= 60:
-#                 savefile.write('>'+seqid+' '+des+'\n'+seq+'\n')
-#             else:
-#                 savefile.write('>'+seqid+' '+des+'\n')
-#                 while len(seq) > 60:
-#                     savefile.write(seq[:60]+'\n')
-#                     seq = seq[60:]
-#                 savefile.write(seq+'\n')
-#     else:
-#         for seqid in fadic:
-#             seq = fadic[seqid]
-#             des = ''
-#             if seqid in description:
-#                 des = description[seqid]
-#             if len(seq) <= 60:
-#                 savefile.write('>'+seqid+' '+des+'\n'+seq+'\n')
-#             else:
-#                 savefile.write('>'+seqid+' '+des+'\n')
-#                 while len(seq) > 60:
-#                     savefile.write(seq[:60]+'\n')
-#                     seq = seq[60:]
-#                 savefile.write(seq+'\n')
-#     savefile.close()
+def write(fadic, savepath, orderlist=[],description={}):
+    '''class Write(fadic, savepath, *orderlist=[default []], *description=[default {}])
+    '''
+    from textwrap import wrap
+    fadic = fadic
+    savepath = savepath
+    orderlist = orderlist
+    description=description
 
-def main():
-    a = Fasta('aaa.fa', mode='wraa')
-    print a.fastaPath
-    print a.mode
+    if savepath[-3:] == '.gz':
+        import gzip
+        infile = gzip.open(savepath,'wb')
+    else:
+        infile = open(savepath,'w')
+    if len(orderlist) == 0:
+        orderlist = fadic.keys()
+    for seqid in orderlist:
+        seq = fadic[seqid]
+        des = ''
+        if seqid in description:
+            des = description[seqid]
+        infile.write('>'+seqid+des+'\n')
+        infile.write('\n'.join(wrap(seq,60)) + '\n')
 
-
-if __name__ == '__main__':
-    main()
+    infile.close()
